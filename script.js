999a1da01d53ffc7878e14127687881bee088833
sreeraj <sreerajts098@gmail.com>
2026-05-04 17:47:04 +0530

    Initial commit - portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('fa-bars'); 
+    menuIcon.classList.toggle('fa-xmark'); 
+    navbar.classList.toggle('active');
+}
